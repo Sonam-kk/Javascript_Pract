fa698ed3e22a9c1cb5a13f963b07f3c6d11d515c
Sonam-kk <sonam22@navgurukul.org>
2023-06-22 11:18:01 +0530

    Timeintervel comleted

---
+++ Timming_Function.js
@@ -0,0 +1,4 @@
+// SetTimeOut:This methoed executes a function after waiting a specific number of miliseconds
+// SetTimeIntervel: This methods repets at every giventime interval
+setTimeout(()=>{alert("Hello from set time out");},3000);
+setInterval(()=>{alert("Hello from set time out");},3000);
