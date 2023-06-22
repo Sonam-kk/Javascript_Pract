b026578a505ad98e69e0de38fc1495fbd14e8363
Sonam-kk <sonam22@navgurukul.org>
2023-06-22 11:51:11 +0530

    compelted

--- map_filter.js
+++ map_filter.js
@@ -1,4 +1,9 @@
 // Calculating the area of circal radius is given.
+// Map method
 array=[1,2,3,4,5];
-const output=array.map((i)=>Math.PI*i*i);
-console.log(output);
+const outputmap=array.map((i)=>Math.PI*i*i);
+console.log(outputmap);
+// Filter mathod
+array1=[1,2,3,4,5,12,18,16];
+const outputfilter=array1.map((i)=>i%6===0);
+console.log(outputfilter);
