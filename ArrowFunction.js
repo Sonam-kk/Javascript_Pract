ca02a14c2cf2250805d7f24dc64e0f5e5d35fcbf
Sonam-kk <sonam22@navgurukul.org>
2023-06-22 10:10:29 +0530

    ArrowFunction

---
+++ ArrowFunction.js
@@ -0,0 +1,13 @@
+// Normal fucntions be like:
+function sum(a,b){
+    let sumvalue =a+b;
+    return sumvalue;
+};
+console.log(sum(2,3));
+
+// Arrow function
+let sumarrow=(a,b)=>{
+    let sumvalue =a+b;
+    return sumvalue;
+}
+console.log(sumarrow(4,5));
