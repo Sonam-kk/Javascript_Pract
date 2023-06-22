00d5c9cdab02c73cc590b109cf6498dbfc0fa9b0
Sonam-kk <sonam22@navgurukul.org>
2023-06-22 10:22:38 +0530

    Callbacks

---
+++ Callbacks.js
@@ -0,0 +1,3 @@
+// A callback is a function that paased into another function as an argument
+// High order function in javascript are funtions thatoprates one another 
+// funtion either by taking them as an argument or by returning them. 
