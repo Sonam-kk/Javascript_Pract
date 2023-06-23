0755ea602440f79678a25ffa7403bcb60746d938
Sonam-kk <sonam22@navgurukul.org>
2023-06-23 09:53:48 +0530

    map_fiter_reduce_method_done

--- map_filter.js
+++
@@ -1,9 +0,0 @@
-// Calculating the area of circal radius is given.
-// Map method
-array=[1,2,3,4,5];
-const outputmap=array.map((i)=>Math.PI*i*i);
-console.log(outputmap);
-// Filter mathod
-array1=[1,2,3,4,5,12,18,16];
-const outputfilter=array1.map((i)=>i%6===0);
-console.log(outputfilter);
