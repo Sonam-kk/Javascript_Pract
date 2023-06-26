437ca646239e1442e0272b48a6d8b33a50ce73be
Sonam-kk <135937805+Sonam-kk@users.noreply.github.com>
2023-06-26 12:10:40 +0530

    Delete Timming_Function.js

--- Timming_Function.js
+++
@@ -1,4 +0,0 @@
-// SetTimeOut:This methoed executes a function after waiting a specific number of miliseconds
-// SetTimeIntervel: This methods repets at every giventime interval
-setTimeout(()=>{alert("Hello from set time out");},3000);
-setInterval(()=>{alert("Hello from set time out");},3000);
